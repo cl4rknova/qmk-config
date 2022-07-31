23d8ed7fa84328c93a7e4060262f4d0239e26667
Marc Seibert <marc.seibert@1und1.de>
2022-08-01 01:15:25 +0200

    restructure for using user space

---
+++ users/cl4rknova/cl4rknova.c
@@ -0,0 +1,9 @@
+/*
+ * cl4rknova.c
+ * Copyright (C) 2022 mseibert <mseibert@LXKA-5KSJRG3.united.domain>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "cl4rknova.h"
+#pragma once
